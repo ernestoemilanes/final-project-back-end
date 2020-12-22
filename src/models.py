111525725bc71ccbd8dfb3b1d76c870eac6495ce
ernestoemilanes <ernestoemilanes@gmail.com>
2020-12-22 01:43:53 +0000

    tables

--- src/models.py
+++ src/models.py
@@ -4,16 +4,41 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
+    first_name = db.Column(db.String(250), unique=False, nullable=False)
+    last_name = db.Column(db.String(250), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+            # do not serialize the password, its a security breach
+        }
+
+class UserIntake(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=False, nullable=False)
+    product_name = db.Column(db.String(250), unique=False, nullable=False)
+    nf_calories = db.Column(db.String(250), unique=False, nullable=False)
+    nf_protein = db.Column(db.String(250), unique=False, nullable=False)
+    nf_fats = db.Column(db.String(250), unique=False, nullable=False)
+    nf_saturated_fats = db.Column(db.String(250), unique=False, nullable=False)
+    nf_sugars = db.Column(db.String(250), unique=False, nullable=False)
+    nf_sodium = db.Column(db.String(250), unique=False, nullable=False)
+    nf_dietary_fiber = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<UserIntake %r>' % self.product_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "product_name": self.product_name
             # do not serialize the password, its a security breach
         }
